082e3d104e84dea9dd1d88a4439db4a7cab2e21e
Kamil Chlebuś <kamil.chlebus@etteplan.com>
2023-10-19 17:03:59 +0200

    Add DragGesture to animate image scale and position

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isAnimating: Bool = false
-    @State private var imageScale: CGFloat = 1.0
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+
+    private func resetImageState() {
+        withAnimation(.spring) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
 
     var body: some View {
         NavigationStack {
@@ -20,11 +28,29 @@ struct ContentView: View {
                     .clipShape(.rect(cornerRadius: 10))
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                     .padding()
                     .onTapGesture(count: 2) {
-                        imageScale = imageScale == 1 ? 5 : 1
+                        if imageScale == 1 {
+                            imageScale = 5
+                        } else {
+                            resetImageState()
+                        }
                     }
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             }
             .animation(.linear(duration: 1), value: isAnimating)
             .animation(.spring, value: imageScale)
