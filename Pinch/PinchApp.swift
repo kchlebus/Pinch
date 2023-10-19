aad33db4aadac7dc8c2df8d71851468ac00f12bd
Kamil Chlebuś <kamil.chlebus@etteplan.com>
2023-10-19 14:53:49 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Kamil Chlebuś on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
