4b16f70b83eb1e868b5c688462467d0d8c3c9a18
Kamil Chlebuś <kamil.chlebus@etteplan.com>
2023-10-19 16:40:36 +0200

    Add front cover image with dobule tap gesture and animations

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(.rect(cornerRadius: 10))
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                    .padding()
+                    .onTapGesture(count: 2) {
+                        imageScale = imageScale == 1 ? 5 : 1
+                    }
+            }
+            .animation(.linear(duration: 1), value: isAnimating)
+            .animation(.spring, value: imageScale)
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        .onAppear {
+            isAnimating = true
+        }
     }
 }
 
